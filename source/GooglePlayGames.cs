f18e88f8ae7b9c2e1af346313e388aa03b157e74
CanRanBan <canranban@gmx.de>
2023-06-03 12:03:51 +0200

    Add fallback for installation path and data path

--- source/GooglePlayGames.cs
+++ source/GooglePlayGames.cs
@@ -14,12 +14,27 @@ namespace GooglePlayGamesLibrary
         {
             get
             {
+                string dataPath;
+
                 using (var key = Registry.LocalMachine.OpenSubKey(@"Software\Google\Play Games"))
                 {
                     if (key?.GetValueNames().Contains("UserLocalAppDataRoot") == true)
                     {
-                        return key.GetValue("UserLocalAppDataRoot")?.ToString() + @"\Google\Play Games";
+                        var rootPath = key.GetValue("UserLocalAppDataRoot")?.ToString();
+                        dataPath = Path.Combine(rootPath, "Google", "Play Games");
+                        if (Directory.Exists(dataPath))
+                        {
+                            return dataPath;
+                        }
                     }
+                    }
+
+                // Fallback to default location if registry key is missing.
+                var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                dataPath = Path.Combine(localAppData, "Google", "Play Games");
+                if (Directory.Exists(dataPath))
+                {
+                    return dataPath;
                 }
 
                 return string.Empty;
@@ -30,12 +45,26 @@ namespace GooglePlayGamesLibrary
         {
             get
             {
+                string installationPath;
+
                 using (var key = Registry.LocalMachine.OpenSubKey(@"Software\Google\Play Games"))
                 {
                     if (key?.GetValueNames().Contains("InstallFolder") == true)
                     {
-                        return key.GetValue("InstallFolder")?.ToString();
+                        installationPath = key.GetValue("InstallFolder")?.ToString();
+                        if (Directory.Exists(installationPath))
+                        {
+                            return installationPath;
+                        }
                     }
+                    }
+
+                // Fallback to default location if registry key is missing.
+                var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+                installationPath = Path.Combine(programFiles, "Google", "Play Games");
+                if (Directory.Exists(installationPath))
+                {
+                    return installationPath;
                 }
 
                 return string.Empty;
